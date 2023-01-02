57d5e2a124f08af2c84afb15c1fb97cd988ad420
coderon_lee <1710034886@qq.com>
2023-01-02 12:38:46 +0800

    [add]structs

--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -26,12 +26,19 @@ impl Package {
         }
     }
 
-    fn is_international(&self) -> ??? {
+    fn is_international(&self) -> bool {
         // Something goes here...
+        if self.sender_country == self.recipient_country {
+            false
+        }
+        else {
+            true
+        }
     }
 
-    fn get_fees(&self, cents_per_gram: i32) -> ??? {
+    fn get_fees(&self, cents_per_gram: i32) -> i32 {
         // Something goes here...
+        self.weight_in_grams*cents_per_gram
     }
 }
 
