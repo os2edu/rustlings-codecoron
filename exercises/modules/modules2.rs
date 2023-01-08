bde97dfaf864d68c9fccfbaf57fd0ee493ea6f9d
a锦龙 <1710034886@qq.com>
2023-01-08 05:40:43 +0000

    [add]modules

--- exercises/modules/modules2.rs
+++ exercises/modules/modules2.rs
@@ -7,8 +7,8 @@
 
 mod delicious_snacks {
     // TODO: Fix these use statements
-    use self::fruits::PEAR as ???
-    use self::veggies::CUCUMBER as ???
+    pub use self::fruits::PEAR as fruit;
+    pub use self::veggies::CUCUMBER as veggie;
 
     mod fruits {
         pub const PEAR: &'static str = "Pear";
@@ -20,6 +20,7 @@ mod delicious_snacks {
         pub const CARROT: &'static str = "Carrot";
     }
 }
+
 
 fn main() {
     println!(
