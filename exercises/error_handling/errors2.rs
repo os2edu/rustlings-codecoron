1119dbeb83470e1fd41b3ebb9c6ee3a7a1e88aa2
a锦龙 <1710034886@qq.com>
2023-01-15 14:07:22 +0000

    [add]errors1-2

--- exercises/error_handling/errors2.rs
+++ exercises/error_handling/errors2.rs
@@ -21,11 +21,21 @@
 
 use std::num::ParseIntError;
 
+// pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
+//     let processing_fee = 1;
+//     let cost_per_item = 5;
+//     let qty = item_quantity.parse::<i32>();
+//     let my_qty = match qty {
+//             Ok(change) => change,
+//             Err(e) => return Err(e),
+//     };
+//     Ok(my_qty * cost_per_item + processing_fee)
+// }
+
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let processing_fee = 1;
     let cost_per_item = 5;
-    let qty = item_quantity.parse::<i32>();
-
+    let qty = item_quantity.parse::<i32>()?;
     Ok(qty * cost_per_item + processing_fee)
 }
 
