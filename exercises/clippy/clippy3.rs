a543aac3ae9f3c9aece4bf8cb74e4adeb2764f10
a锦龙 <1710034886@qq.com>
2023-05-05 10:42:18 +0000

    [add]clippy1-3

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -6,23 +6,29 @@
 #[allow(unused_variables, unused_assignments)]
 fn main() {
     let my_option: Option<()> = None;
-    if my_option.is_none() {
-        my_option.unwrap();
-    }
+    //because of this check,my_option.unwrap() will panic
+    // if my_option.is_none() {
+    //     my_option.unwrap();
+    // }
 
     let my_arr = &[
-        -1, -2, -3
+        -1, -2, -3,
         -4, -5, -6
     ];
     println!("My array! Here it is: {:?}", my_arr);
 
-    let my_empty_vec = vec![1, 2, 3, 4, 5].resize(0, 5);
+//Checks for binding a unit value.
+//A unit value cannot usefully be used anywhere. So binding one is kind of pointless.
+//TODO reset(0,5) return what?
+// return ()
+    let mut my_empty_vec = vec![1, 2, 3, 4, 5].resize(0, 5);
     println!("This Vec is empty, see? {:?}", my_empty_vec);
 
     let mut value_a = 45;
     let mut value_b = 66;
     // Let's swap these two!
-    value_a = value_b;
-    value_b = value_a;
+    std::mem::swap(&mut value_a, &mut value_b);
+    // value_a = value_b;
+    // value_b = value_a;
     println!("value a: {}; value b: {}", value_a, value_b);
 }
